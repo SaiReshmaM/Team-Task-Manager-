d088aef666748983e397b4c49b5e999e806d8128
SaiReshmaM <reshmamaddu@gmail.com>
2026-05-02 00:04:33 +0530

    commit  code

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080'
+    baseURL: 'https://team-task-manager-production-a389.up.railway.app'
 });
 
 api.interceptors.request.use(

43cd676b5af65212765f95ba5036b7dd79ac5e6e
SaiReshmaM <reshmamaddu@gmail.com>
2026-05-02 00:18:10 +0530

    commit code

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,18 @@
-import { defineConfig } from 'vite'
+// import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
   server: {
+    host: true,
+    port: 5173,
     allowedHosts: 'all',
   },
+
   preview: {
+    host: true,
+    port: process.env.PORT || 5173,
     allowedHosts: 'all',
   },
 })
